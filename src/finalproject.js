1f407da169d1cc9afb45b4cdbb47c034593bc137
sofieb18 <98337235+sofieb18@users.noreply.github.com>
2022-04-06 21:53:47 +0100

    update

--- src/finalproject.js
+++
@@ -1,8 +0,0 @@
-function apply() {
-  let firstName = prompt("What is your first name?");
-  alert("Hi " + firstName + ", enjoy your trip to Japan!");
-}
-
-let applyButton = document.querySelector("button");
-
-applyButton.addEventListener("click", apply);
